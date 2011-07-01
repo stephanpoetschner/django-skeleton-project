2bf894133844e090b95344d03487cf5b161bc50d
Stephan Pötschner <stephan.poetschner@gmail.com>
2011-07-01 15:23:31 +0200

    + default logging added

--- settings.py
+++ settings.py
@@ -30,12 +30,6 @@ DATABASES = {
     }
 }
 
-# logging.basicConfig(
-#     level = logging.DEBUG,
-#     format = '%(asctime)s %(levelname)s %(message)s',
-#     filename = '/tmp/djangoproject.log',
-#     filemode = 'w'
-# )
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -187,6 +181,31 @@ EMAIL_HOST_USER = ''
 EMAIL_PORT = 25
 # Whether to use a TLS (secure) connection when talking to the SMTP server.
 EMAIL_USE_TLS = False
+
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
